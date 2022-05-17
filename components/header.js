4e911e489c0c606c3e1e6b039e20f8c6bdc66b02
Sichu0725 <choihc8511@gmail.com>
2022-05-18 08:51:36 +0900

    css 및 root 경로 설정

--- components/header.js
+++ components/header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from 'next/link'
-import "../styles/header.module.scss"
+import style from "../styles/header.module.scss"
 import { WbSunny, Menu, Search } from '@mui/icons-material';
 export const Header = () => {
     return(
-        <div className="header">
-            <div className="left">
-                <ul className="list">
-                    <li className="localName">Useong</li>
+        <div className={style.header}>
+            <div className={style.left}>
+                <ul className={style.list}>
+                    <li className={style.localName}>Useong</li>
                     <li className="nowTab">news</li>
                     <li className="nowDate">화요일</li>
                     <li className="nowTime">12:11:52</li>
@@ -18,8 +18,8 @@ export const Header = () => {
                 </ul>
             </div>
             <div>
-                <Menu className="lightIcon icon" onClick={()=>{console.log("click Menu!")}} />
-                <Search className="lightIcon icon" onClick={()=>{console.log("click Search")}}/>
+                <Menu className={`${style.lightIcon} ${style.icon}`} onClick={()=>{console.log("click Menu!")}} />
+                <Search className={`${style.lightIcon} ${style.icon}`} onClick={()=>{console.log("click Search")}}/>
             </div>
         </div>
     )
